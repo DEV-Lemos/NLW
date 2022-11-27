f18e613dddcc7d947a4b186861f8a4097ff78246
DEV-Lemos <isac.lemos@outlook.com>
2022-11-26 21:09:27 -0300

    Primeiro

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,67 @@
+import problemaImageUrl from "../../assets/problema.png";
+import ideiaImageUrl from "../../assets/ideia.png";
+import outroImageUrl from "../../assets/outro.png";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: problemaImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideiaImageUrl,
+      alt: "Imagem de uam Lampada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: outroImageUrl,
+      alt: "Imagem de balão de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false)
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com amor pelo Isac
+      </footer>
+    </div>
+  );
+}
